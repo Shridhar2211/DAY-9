9f4c8dad24b678cc8a60b425b0f593099d360142
Shridhar  Kolhatkar <88042097+Shridhar2211@users.noreply.github.com>
2021-10-12 17:33:40 +0530

    Add: initial commit

--- AddressBook.java
+++ AddressBook.java
@@ -1,66 +1,112 @@
-package AdressBook;
+package javasample;
 
-public class AdressBookUC1 {
-	//Declaration part
-		String FirstName;
-		String LastName;
-		String Adress;
-		String City;
-		String State;
-		int Zip;
-		int PhoneNumber;
-		String Email;
-		//constructor is created
-		 public AdressBookUC1(String FirstName, String LastName, String Adress, String City, int Zip,
-	             String State, int PhoneNumber, String Email) {
+
+public class AddressBookUC1 {
+	 // Main method
+	public static void main(String args[]) {
+		 Contact person = new Contact();
 		
-			 
-			    this.FirstName = FirstName;
-			    
-			    
-		        this.LastName = LastName;
-		       
-		        
-		        this.Adress = Adress;
-		        
-		        
-		        this.City = City;
-		        
-		        
-		        this.Zip = Zip;
-		        
-		        
-		        this.State = State;
-		        
-		        
-		        this.PhoneNumber = PhoneNumber;
-		        
-		        
-		        this.Email = Email;
-		        
-		 }
-		 //function Declare
-		    public void details(){
-		        System.out.println("FirstName : " + FirstName);
-		        System.out.println("Lastame : " + LastName);
-		        System.out.println("Adress : " + Adress);
-		        System.out.println("City : " + City);
-		        System.out.println("Zip : " + Zip);
-		        System.out.println("State : " + State);
-		        System.out.println("PhoneNumber : " + PhoneNumber);
-		        System.out.println("Email : " + Email);
-		    }
-		 //main function
-		 
-		public static void main(String[] args) {
-			
-			System.out.println("Welcome to the Address Book");
-	        AdressBookUC1 contacts = new AdressBookUC1("Shridhar","Kolhatkar","Gokulnager",
-	        "nanded",431801,"Maharastra",1234567890,"shridharkolhatkar@gmail.com");
-	       contacts.details();
-
-	     }
+		person.setFirstname("shridhar");
+		person.setLastname("kolhatkar");
+		person.setCity("nanded");
+		person.setEmail("shrigmail.com");
+		person.setAddress("kalepadal Pune");
+		person.setState("Maharashtra");
+		person.setZip(410056);
+		person.setPhonenumber(1234567890); 
 		
+		System.out.println(person);
+	 }
+}
+     
+package javasample;
 
+public class Contact {
+  //variable declare
+	
+    public  String lastname;
+    public  String address;
+    public  String city;
+    public  String state;
+    public  long zip;
+    public  long phonenumber;
+    public  String email;
+    
+    
+    //Generate getter setter method
+    
+	
+	public String getFirstname() {
+		return firstname;
 	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public long getZip() {
+		return zip;
+	}
+	public void setZip(long zip) {
+		this.zip = zip;
+	}
+	public long getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(long phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+//Generate String method	
+    public String firstname;
+    @Override
+	public String toString() {
+		return "Contact [firstname=" + firstname + ", lastname=" + lastname + ", address=" + address + ", city="
+				+ city + ", state=" + state + ", zip=" + zip + ", phonenumber=" + phonenumber + ", email=" + email
+				+ "]";
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
